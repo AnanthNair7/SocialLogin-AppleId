ddf1cffd9c4ba37dfac6fe9320bafc746bc9db57
Ananth Nair <ananthnair96@gmail.com>
2023-12-12 21:09:35 +0530

    Initial Commit

---
+++ SocialLogin-appleId/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLogin-appleId
+//
+//  Created by Ananth Nair on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
