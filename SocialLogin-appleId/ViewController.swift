c1f8d90b803f31229dc99ddcc46b55ee71c8fff2
Ananth Nair <ananthnair96@gmail.com>
2023-12-28 12:45:37 +0530

    inital commit for socail login

--- SocialLogin-appleId/ViewController.swift
+++ SocialLogin-appleId/ViewController.swift
@@ -5,15 +5,59 @@
 //  Created by Ananth Nair on 12/12/23.
 //
 
+import AuthenticationServices
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let authenticateButton = ASAuthorizationAppleIDButton()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(authenticateButton)
+        authenticateButton.addTarget(self, action: #selector(didAuthenticateTapped), for: .touchUpInside)
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        authenticateButton.frame=CGRect(x: 0, y: 0, width: 250, height: 50)
+        authenticateButton.center = view.center
+    }
+    
+    @objc func  didAuthenticateTapped() {
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+        
+    }
+    
+    
+}
+extension ViewController : ASAuthorizationControllerDelegate{
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Failed")
+    }
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let credentials as ASAuthorizationAppleIDCredential :
+            let firstName = credentials.fullName?.givenName
+            let lastName = credentials.fullName?.familyName
+            let email = credentials.email
+            break
+        default :
+            break
+        }
+    }
+}
 
-
+extension ViewController : ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+    
 }
 
